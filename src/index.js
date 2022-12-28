e9f4b1e56a2c9fae2f99a9248fede4de548bfa50
ctang415 <carrietang117@gmail.com>
2022-12-28 16:29:39 -0500

    Created function to return an array of possible moves when given a position on an 8 by 8 board

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+ const knightArray = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, -1], [-2, 1]]
+
+ const getMoves = (currentPosition) => {
+    const possibleMoves = [];
+    for (let element of knightArray) {
+        const first = currentPosition[0] + element[0]
+        const second = currentPosition[1] + element[1]
+        if (first <= 7 && second <= 7) {
+        possibleMoves.push([first, second])
+        }
+    }
+    console.log(possibleMoves)
+ }
+ getMoves([3, 6])
