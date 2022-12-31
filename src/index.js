293ff92b4beaca2b7d340d8e52bc6b2ae0e0e7d1
ctang415 <carrietang117@gmail.com>
2022-12-31 13:39:44 -0500

    Created function that will return the amount of moves and move set to reach destination from current position

--- src/index.js
+++ src/index.js
@@ -13,4 +13,47 @@ import './style.css';
     }
     console.log(possibleMoves)
  }
- getMoves([3, 6])
+
+ 
+ const knightMoves = (currentPosition, finalPosition) => {
+    const queue = [currentPosition]
+    const visited = []
+    const obj = {};
+    while (queue.length !== 0) {
+        const currentPosition = queue.shift()
+        const positionString = `${currentPosition[0]},${currentPosition[1]}`
+        if (Object.keys(obj).length === 0) {
+            obj[positionString] = 'No parent'
+        }
+        visited.push(positionString)
+        if (currentPosition[0] === finalPosition[0] && currentPosition[1] === finalPosition[1]) {
+            return backtrackMoves(obj, currentPosition)
+        }
+        for (let element of knightArray) {
+            const xCoordinate = currentPosition[0] + element[0]
+            const yCoordinate = currentPosition[1] + element[1]
+            const newCoord = `${xCoordinate},${yCoordinate}`
+            if ((xCoordinate >= 0 && xCoordinate <= 7) && (yCoordinate >= 0 && yCoordinate <= 7)) {
+                if (!visited.includes(newCoord)) {
+                    obj[newCoord] = currentPosition
+                    queue.push([xCoordinate, yCoordinate])
+                    }
+                }
+            }
+        }
+    }
+
+    const backtrackMoves = (obj, currentPosition) => {
+        const fastestPath = [];
+        while (obj[currentPosition]) {
+            fastestPath.push(currentPosition)
+            currentPosition = obj[currentPosition]
+        }
+        const fastestPathReverse = fastestPath.reverse();
+        const fastestPathMoves = fastestPath.length - 1
+        console.log("=> You made it in " + `${fastestPathMoves}` + " moves. Here is your path: ")
+        fastestPathReverse.forEach(element => console.log(element))
+    }
+
+
+knightMoves([0,0], [7,4])
