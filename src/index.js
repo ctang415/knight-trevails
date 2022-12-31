9895f82d4b5d2be4e5f3e1a771fa14b9cce79c18
ctang415 <carrietang117@gmail.com>
2022-12-31 13:40:42 -0500

    Cleaned up old code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import './style.css';
 
  const knightArray = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, -1], [-2, 1]]
-
- const getMoves = (currentPosition) => {
-    const possibleMoves = [];
-    for (let element of knightArray) {
-        const first = currentPosition[0] + element[0]
-        const second = currentPosition[1] + element[1]
-        if (first <= 7 && second <= 7) {
-        possibleMoves.push([first, second])
-        }
-    }
-    console.log(possibleMoves)
- }
-
  
  const knightMoves = (currentPosition, finalPosition) => {
     const queue = [currentPosition]
