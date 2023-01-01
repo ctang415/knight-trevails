38e4b60c7854de1ce679e7b42358e2c90cca1256
ctang415 <carrietang117@gmail.com>
2023-01-01 13:52:00 -0500

    Added clicks to chessboard that will register first move and final destination

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import './style.css';
  const knightArray = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, -1], [-2, 1]]
  const container = document.querySelector('.container')
 
+ class Knight {
+    constructor(currentPosition, finalPosition) {
+        this.currentPosition = []
+        this.finalPosition = []
+    }
+    clearPositions() {
+        this.currentPosition = []
+        this.finalPosition = []
+    }
+}
+
+const knightPiece = new Knight;
+
  const knightMoves = (currentPosition, finalPosition) => {
     const queue = [currentPosition]
     const visited = []
@@ -15,6 +28,7 @@ import './style.css';
         }
         visited.push(positionString)
         if (currentPosition[0] === finalPosition[0] && currentPosition[1] === finalPosition[1]) {
+            knightPiece.clearPositions()
             return backtrackMoves(obj, currentPosition)
         }
         for (let element of knightArray) {
@@ -38,6 +52,7 @@ import './style.css';
             currentPosition = obj[currentPosition]
         }
         const fastestPathReverse = fastestPath.reverse();
+        console.log(fastestPath)
         const fastestPathMoves = fastestPath.length - 1
         console.log("=> You made it in " + `${fastestPathMoves}` + " moves. Here is your path: ")
         fastestPathReverse.forEach(element => console.log(element))
@@ -47,9 +62,22 @@ import './style.css';
         for (let i = 0; i < col ; i++){
             for (let j = 0; j < row; j++) {
             let div = document.createElement('div');
+            div.setAttribute('data-id', [`${i}, ${j}`])
             if ((i + j) % 2 === 0) {
                 div.style.backgroundColor = '#EDE0B2'
             }
+            div.addEventListener('click', () => {
+                if (knightPiece.currentPosition.length === 0) {
+                    const positionX = parseInt(div.getAttribute('data-id')[0])
+                    const positionY = parseInt(div.getAttribute('data-id')[3])
+                    knightPiece.currentPosition.push(positionX, positionY)
+                } else {
+                    const positionX = parseInt(div.getAttribute('data-id')[0])
+                    const positionY = parseInt(div.getAttribute('data-id')[3])
+                    knightPiece.finalPosition.push(positionX, positionY)
+                    knightMoves(knightPiece.currentPosition, knightPiece.finalPosition)
+                }
+            })
             container.appendChild(div).className = "div";
             }
         }
